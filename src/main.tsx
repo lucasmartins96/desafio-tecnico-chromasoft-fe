2d16aacac0ddb79ab2f7a8a554b2e76ed478c7db
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2025-03-25 22:04:22 -0300

    refactor(main): adicionar SnackbarProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router';
+import { SnackbarProvider } from 'notistack';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <SnackbarProvider
+        anchorOrigin={{
+          horizontal: 'center',
+          vertical: 'top',
+        }}
+      >
+        <App />
+      </SnackbarProvider>
     </BrowserRouter>
   </StrictMode>
 );

f052cfd1a4e220e9bb6726ea8c8e6fb29e4d216c
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2025-03-28 14:13:19 -0300

    refactor(axios): definir api host por variável de ambiente

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_HOST = import.meta.env.VITE_API_HOST;
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: `${API_HOST}/api/v1`,
 });
 
 axiosInstance.interceptors.response.use(

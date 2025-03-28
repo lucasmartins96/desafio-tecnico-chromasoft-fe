ef49d54dafa922adccef4da5e911e5fff17185d5
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2025-03-28 08:09:38 -0300

    feat(axios): adicionar interceptador na instância do axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -4,4 +4,28 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000/api/v1',
 });
 
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response) {
+      const { status } = error.response;
+
+      if (status === 401) {
+        console.error('Token inválido ou expirado. Redirecionando para login...');
+
+        window.location.href = '/';
+
+        return Promise.reject(error);
+      }
+
+      if (status === 403) {
+        console.error('Acesso negado. Você não tem permissão para acessar este recurso.');
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;

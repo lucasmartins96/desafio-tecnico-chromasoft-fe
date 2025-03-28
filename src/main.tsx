3cfc9f5a21c82b6df6018558288bafbdec2ab34d
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2025-03-28 08:13:07 -0300

    refactor(main): atualizar o nome arquivo de TasksPage

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { SnackbarProvider } from 'notistack';
-import TasksPage from './pages/tasks.tsx';
+import TasksPage from './pages/TasksPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
